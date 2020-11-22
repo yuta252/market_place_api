4ab0c48c8eb045adf0906040f5db792c39b48fde
Yuta Nakano <nakano.yuta252@gmail.com>
2020-11-22 17:50:12 +0900

    Adds methods for the orders controller

---
+++ app/controllers/api/v1/orders_controller.rb
@@ -0,0 +1,33 @@
+class Api::V1::OrdersController < ApplicationController
+  before_action :check_login, only: [:index, :show, :create]
+
+  def index
+    render json: OrderSerializer.new(current_user.orders).serializable_hash
+  end
+
+  def show
+    order = current_user.orders.find(params[:id])
+    if order
+      options = { include: [:products] }
+      render json: OrderSerializer.new(order, options).serializable_hash
+    else
+      head 404
+    end
+  end
+
+  def create
+    order = current_user.orders.build(order_params)
+
+    if order.save
+      render json: order, status: 201
+    else
+      render json: { errors: order.errors }, status: 422
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:total, product_ids: [])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      resources :orders, only: [:index, :show, :create]
       resources :users, only: [:show, :create, :update, :destroy]
       resources :tokens, only: [:create]
       resources :products
